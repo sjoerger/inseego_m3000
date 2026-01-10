fe43421dfdfcb02edf1ad8546cd80536e3c23c15
Steve Joerger <github@joerger.us>
2026-01-09 23:17:10 -0500

    Remove manually deleted files.

--- const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Inseego M3000 Hotspot integration."""
-
-DOMAIN = "inseego_m3000"
-DEFAULT_SCAN_INTERVAL = 30  # seconds
-DEFAULT_PORT = 80
-DEFAULT_TIMEOUT = 10
-
-CONF_HOST = "host"
